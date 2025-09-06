15ea39ea172dcf476cf545ea207e0e895eeb8aee
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-09-06 20:07:52 +0100

    Merge dev to main (#14)
    * Added FX Conversion Data Model
    
    * Added flake8 dependency
    
    * Fixed linting issues
    
    * Fixed init files
    
    * Fixed unit test executions
    
    * Fixed release (#7)
    
    * Fixed release (#9)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing (#11)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing
    
    * Fixed release pipeline (#13)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing
    
    * Fixed release pipeline

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="equicast_pyutils",
-    version="0.1",
-    packages=find_packages(),
+    version="0.1.0",
+    packages=find_packages(exclude=("tests", "tests.*")),
+    long_description=long_description,
     url="https://github.com/coldsofttech/equiCast-pyutils",
     license="MIT",
     author="coldsofttech",
     install_requires=requirements,
-    requires_python=">=3.10",
-    setup_requires=["setuptools-git-versioning"]
+    python_requires=">=3.10",
+    setup_requires=["setuptools-git-versioning"],
+    classifiers=[
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
 )
