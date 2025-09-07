204687d529e82b9744a9a4c8ad6d0d55168b6be4
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-09-07 19:52:45 +0100

    Updated Release

--- equicast_pyutils/__init__.py
+++ equicast_pyutils/__init__.py
@@ -1,3 +1,10 @@
 __all__ = [
     "models"
 ]
+
+import os
+import sys
+
+_vendor_path = os.path.join(os.path.dirname(__file__), "_vendor")
+if _vendor_path not in sys.path:
+    sys.path.insert(0, _vendor_path)
