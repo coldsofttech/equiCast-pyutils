346ac10789cd2de1e35fb4a559fe9e5ddd4907a1
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-09-07 09:32:32 +0100

    Fixed release pipeline (#17)
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing
    
    * Fixed release pipeline
    
    * Added release pipeline
    
    * Fixed release pipeline

--- setup.py
+++ setup.py
@@ -1,25 +1,9 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="equicast_pyutils",
-    version="0.1.0",
     packages=find_packages(exclude=("tests", "tests.*")),
-    long_description=long_description,
-    url="https://github.com/coldsofttech/equiCast-pyutils",
-    license="MIT",
-    author="coldsofttech",
-    install_requires=requirements,
-    python_requires=">=3.10",
-    setup_requires=["setuptools-git-versioning"],
-    classifiers=[
-        "Programming Language :: Python :: 3.13",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ]
+    long_description=long_description
 )
