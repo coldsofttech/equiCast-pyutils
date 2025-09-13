bb517a04dc6e61b2d0017d6cea4e7745c938343d
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-09-13 17:15:00 +0100

    Merge dev to main (#45)
    * Added FX Conversion Data Model
    
    * Added flake8 dependency
    
    * Fixed linting issues
    
    * Fixed init files
    
    * Fixed unit test executions
    
    * Fixed release (#7)
    
    * Fixed release (#9)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing (#11)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing
    
    * Fixed release pipeline (#13)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing
    
    * Fixed release pipeline
    
    * Added release pipeline (#15)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing
    
    * Fixed release pipeline
    
    * Added release pipeline
    
    * Fixed release pipeline (#17)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing
    
    * Fixed release pipeline
    
    * Added release pipeline
    
    * Fixed release pipeline
    
    * Added FxDataExtractor (#20)
    
    * Updated Release (#25)
    
    * Removed delete-all-revisions (#27)
    
    * Updated Release
    
    * Removed delete-all-revisions
    
    * Increased version (#29)
    
    * Updated Release
    
    * Removed delete-all-revisions
    
    * Increased version
    
    * Added StockPriceModel and StockDataExtractor (#32)
    
    * Added dividend to StockDataExtractor (#36)
    
    * Merge dev to main (#33)
    
    * Added FX Conversion Data Model
    
    * Added flake8 dependency
    
    * Fixed linting issues
    
    * Fixed init files
    
    * Fixed unit test executions
    
    * Fixed release (#7)
    
    * Fixed release (#9)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing (#11)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing
    
    * Fixed release pipeline (#13)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing
    
    * Fixed release pipeline
    
    * Added release pipeline (#15)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing
    
    * Fixed release pipeline
    
    * Added release pipeline
    
    * Fixed release pipeline (#17)
    
    * Fixed release
    
    * Fixed release
    
    * Added token for package publishing
    
    * Fixed release pipeline
    
    * Added release pipeline
    
    * Fixed release pipeline
    
    * Added FxDataExtractor (#20)
    
    * Updated Release (#25)
    
    * Removed delete-all-revisions (#27)
    
    * Updated Release
    
    * Removed delete-all-revisions
    
    * Increased version (#29)
    
    * Updated Release
    
    * Removed delete-all-revisions
    
    * Increased version
    
    * Added StockPriceModel and StockDataExtractor (#32)
    
    * Added dividend to StockDataExtractor
    
    * Added enhancements (#39)
    
    1) Empty property for Models
    2) is_delisted for StockExtractor
    3) Simplified pipeline logic by introducing reusable Python scripts
    4) Added tests
    
    * Update __init__.py
    
    * Added AWS Utils (#43)
    
    * Update release.yml (#44)
    
    * Added AWS Utils
    
    * Update release.yml
    
    * Deleted scripts folder
    
    * Feature/42 utilise aws utils in python utils (#46)
    
    * Added AWS Utils
    
    * Update release.yml
    
    * Added Scripts and removed tests

--- scripts/calculate_codeartifact_cost.py
+++ scripts/calculate_codeartifact_cost.py
@@ -1,57 +1,12 @@
-import os
 from pathlib import Path
 
-RATE_PER_GB = 0.05  # USD
-THRESHOLD = 1.0  # USD for coloring
-
-
-def calculate_cost(total_bytes: int):
-    total_gb = total_bytes / (1024 ** 3)
-    cost = total_gb * RATE_PER_GB
-    color = "red" if cost > THRESHOLD else "green"
-    return total_gb, cost, color
-
-
-def get_total_bytes(artifact_dir: Path):
-    artifacts = list(artifact_dir.glob("*"))
-    if not artifacts:
-        raise FileNotFoundError("No artifacts found")
-    return sum(f.stat().st_size for f in artifacts)
-
-
-def write_github_outputs(total_gb: float, cost: float):
-    github_output = os.environ.get("GITHUB_OUTPUT")
-    if github_output:
-        with open(github_output, "a", encoding="utf-8") as f:
-            f.write(f"artifact_size_gb={total_gb:.6f}\n")
-            f.write(f"estimate_cost={cost:.6f}\n")
-
-
-def write_github_summary(total_gb: float, cost: float, color: str):
-    summary_path = os.environ.get("GITHUB_STEP_SUMMARY")
-    if summary_path:
-        with open(summary_path, "a", encoding="utf-8") as f:
-            f.write("### 💰 CodeArtifact Cost Estimate\n")
-            f.write("| Resource     | Storage Size (GB) | Estimated Monthly Cost |\n")
-            f.write("|--------------|-------------------|------------------------|\n")
-            f.write(f"| CodeArtifact | {total_gb:.6f} | <span style='color:{color}'>${cost:.6f}</span> |\n")
+from equicast_awsutils.cost import CodeArtifact
 
 
 def main():
-    artifact_dir = Path("dist")
-    try:
-        total_bytes = get_total_bytes(artifact_dir)
-    except FileNotFoundError as e:
-        print(e)
-        exit(1)
-
-    total_gb, cost, color = calculate_cost(total_bytes)
-
-    print(f"Total Artifact Size (GB): {total_gb:.6f}")
-    print(f"Estimated CodeArtifact Storage Cost: ${cost:.6f} ({color})")
-
-    write_github_summary(total_gb, cost, color)
-    write_github_outputs(total_gb, cost)
+    folder_path = Path(__file__).parent.parent / "dist"
+    ca = CodeArtifact(folder=folder_path)
+    ca.calculate()
 
 
 if __name__ == "__main__":
