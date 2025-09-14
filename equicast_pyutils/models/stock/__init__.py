f41d90f39a1d9de7d7a69c2073266863cea7dd61
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-09-14 11:26:22 +0100

    Added company profile (#51)

--- equicast_pyutils/models/stock/__init__.py
+++ equicast_pyutils/models/stock/__init__.py
@@ -1,7 +1,11 @@
 __all__ = [
-    "StockPriceModel",
-    "DividendModel"
+    "CompanyProfileModel",
+    "CompanyAddressModel",
+    "CompanyOfficerModel",
+    "DividendModel",
+    "StockPriceModel"
 ]
 
+from .company_profile_model import CompanyProfileModel, CompanyAddressModel, CompanyOfficerModel
 from .dividend_model import DividendModel
 from .stock_price_model import StockPriceModel
